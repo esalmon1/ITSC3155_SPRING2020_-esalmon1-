96a977b6c3c76bd231d10b909b0db51e1f4a4111
ted salmon <esalmon1@uncc.edu>
2021-09-28 09:05:30 -0400

    PythonBasics 3 - added test cases to test_starts_with_non_number

--- PythonBasics3/pythonBasics3Test.py
+++ PythonBasics3/pythonBasics3Test.py
@@ -18,6 +18,9 @@ class TestPythonBasicsOne(unittest.TestCase):
         self.assertEqual(pythonBasics3.starts_with_non_number("# is used to comment a line in Python"), True)
 
         # Please add three more test cases following the order above
+        self.assertEqual(pythonBasics3.starts_with_non_number("5 houses in my neighborhood"), False)
+        self.assertEqual(pythonBasics3.starts_with_non_number("I woke up today"), True)
+        self.assertEqual(pythonBasics3.starts_with_non_number("6 is my favorite number"), False)
 
 #Test case for multiple_words
     def test_multiple_words(self):
