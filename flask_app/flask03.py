0ce19cd5a64c6aa16699b19f7557538dc1a3fd0f
esalmon1 <esalmon1@uncc.edu>
2021-10-26 11:34:08 -0400

    changed file

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -1,22 +1,29 @@
 # FLASK Tutorial 1 -- We show ithe bare bones code to get an app up and running
 
-# imports
-import os                 # os is used to get environment variables IP & PORT
+import os                # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 app = Flask(__name__)     # create an app
-
+notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
+            2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'}
+             }
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
 @app.route('/index')
 def index():
     a_user = {'name': 'Ted', 'email': 'esalmon1@uncc.edu'}
-
-    return render_template("index.html", user = a_user)
+    return render_template('index.html', user= a_user)
 
 @app.route('/notes')
 def get_notes():
-    notes = {1: {'title': 'First note', 'text': 'This is my first note', 'date': '10-1-2020'},
-             2: {'title': 'Second note', 'text': 'This is my second note', 'date': '10-2-2020'}
+    a_user = {'name': 'Ted', 'email': 'esalmon1@uncc.edu'}
+    return render_template('new.html', user=a_user)
+
+@app.route('/notes/<notes_id>')
+def get_note(note_id):
+        a_user = {'name': 'Ted', 'email': 'esalmon1@uncc.edu'}
+        return render_template('new.html', note=notes[int(note_id)], user=a_user)
+
+
 
